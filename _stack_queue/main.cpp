06dc8a4b57ef9441fdb90101e73aa4ac4e8694fe
ChenMiaoi <3026066798@qq.com>
2022-03-12 17:07:05 +0800

    Finished the stack container

--- _stack_queue/main.cpp
+++ _stack_queue/main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include "Stack.h"
 
-using namespace std;
+namespace mine_stack
+{
+	void test1()
+	{
+		stack<int> st;
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+
+		while (!st.empty())
+		{
+			cout << st.top() << " ";
+			st.pop();
+		}
+		cout << endl;
+	}
+}
 
 int main()
 {
-	cout << "hello world" << endl;
+	mine_stack::test1();
 }
