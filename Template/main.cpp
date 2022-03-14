fd77399cf88eff82b94f095a66fead15a8f16890
ChenMiaoi <3026066798@qq.com>
2022-03-14 18:40:10 +0800

    just a deque container test code

---
+++ Template/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+//void test_deque()
+//{
+//	deque<int> dq;
+//	dq.push_back(1);
+//	dq.push_back(2);
+//	dq.push_back(3);
+//	dq.push_back(4);
+//	dq.push_front(5);
+//	dq.push_front(6);
+//	dq.push_front(7);
+//	dq.push_front(8);
+//
+//	for (size_t i = 0; i < dq.size(); i++)
+//	{
+//		cout << dq[i] << " ";
+//	}
+//	cout << endl;
+//
+//	deque<int>::iterator it = dq.begin();
+//	while (it != dq.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//	 
+//	for (auto e : dq)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//}
+
+ 
+
+int main()
+{
+	//test_deque();
+}
