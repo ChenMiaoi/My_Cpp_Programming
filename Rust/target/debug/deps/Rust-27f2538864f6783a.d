28d863018a00d43652f254c06a88879a5cead44c
ChenMiaoi <3026066798@qq.com>
2022-07-15 01:06:16 +0800

    The Rust code of I did

---
+++ Rust/target/debug/deps/Rust-27f2538864f6783a.d
@@ -0,0 +1,5 @@
+/home/zack/code_work/github_work/My_Cpp_Programming/Rust/target/debug/deps/Rust-27f2538864f6783a: src/main.rs
+
+/home/zack/code_work/github_work/My_Cpp_Programming/Rust/target/debug/deps/Rust-27f2538864f6783a.d: src/main.rs
+
+src/main.rs:
